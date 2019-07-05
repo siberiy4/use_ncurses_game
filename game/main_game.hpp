11101f6035d794a7a0d51e2ab2695d2b593dc99a
yo-kanyukari <e1n17075@st.oit.ac.jp>
2019-07-05 09:31:43 +0900

    自機機能はそれなりにそろった？

--- game/main_game.hpp
+++ game/main_game.hpp
@@ -9,11 +9,11 @@ void main_game()
     try
     {
         std::thread th_a(own_machine::players_move);
-        //      std::thread th_b(own_machine::players_attack);
+              std::thread th_b(own_machine::players_attack);
         std::thread th_c(own_machine::input);
 
         th_a.join();
-        //     th_b.join();
+             th_b.join();
         th_c.join();
     }
     catch (const std::exception &e)
